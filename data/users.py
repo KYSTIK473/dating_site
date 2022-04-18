bd5457aae192b951a40199fdfb7af6232be03225
KoMzAB <danilkyst@gmail.com>
2022-04-18 23:05:20 +0300

    отклики работают

---
+++ data/users.py
@@ -0,0 +1,32 @@
+import datetime
+import sqlalchemy
+from flask_login import UserMixin
+from sqlalchemy_serializer import SerializerMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = "users"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    city = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    img = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
+    static = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True, default=None)
+    modified_date = sqlalchemy.Column(
+        sqlalchemy.DateTime, default=datetime.datetime.now
+    )
+    news = orm.relation("Responses", back_populates="user")
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
